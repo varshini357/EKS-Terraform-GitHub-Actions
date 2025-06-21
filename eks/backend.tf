2d45a657bdf1ec8ecb0a6133d203fe93f3001ccb
Varshini Koppisetti <varshinikoppisetti999@gmail.com>
2025-06-21 23:49:53 +0530

    Update backend.tf to allow Terraform v1.12.2

--- eks/backend.tf
+++ eks/backend.tf
@@ -8,7 +8,7 @@ terraform {
   }
   backend "s3" {
     bucket         = "eksbuckethima"
-    region         = "us-east-1"
+    region         = "us-west-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
